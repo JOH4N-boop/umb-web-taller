cfb200d493410e2cd94364c31edcfe6395feda5e
Tu Nombre <tu.email@ejemplo.com>
2025-11-17 13:43:32 -0500

    Otro cambio

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
-require_once "modelo.php";
+require_once __DIR__ . '/modelo.php';
+
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 
